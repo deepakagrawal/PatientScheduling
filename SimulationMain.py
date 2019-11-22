correct the doubly demand calculation

SimulationMain.py
@@ -12,6 +12,7 @@ import tempfile
 from gams import *
 from pyutilib.misc import Options
 from scipy import stats
+import warnings
 
 from model_code1 import generateDailyProb
 
@@ -139,9 +140,11 @@ def run_main(iter, data=None):
         [[np.random.poisson(data.ldist[c, loc].rvs(random_state=iter), size=data.Tmax) for loc in range(data.dl)] for c in
          range(data.dc)])
     pat[1,:] = pat[1,:].sum(axis=0) if loc_dep is 1 else pat[1,:]
+    C_temp = np.empty(shape=[data.dk, data.dl,data.Tmax], dtype=int)
     for i in range(data.Tmax):
         if data.dynamic in ["DP","DPP","DCPP"]:
             P = generateDailyProb(data, data.dynamic, 1, schedule, i, loc_dep=loc_dep)
+            C_temp[:,:,i] = data.C
 
         # elif data.dynamic in ["DQP"]: ###### Try with adding future expected appointments
         #     if i>0:
@@ -171,31 +174,34 @@ def run_main(iter, data=None):
                                                                                                     cancel[0, :, tj,c, loc],
                                                                                                     left[c, loc], data.r[0])
                         if patgoing[0,:,tj,c,loc].sum() + left[c,loc] != pat[c,loc,i]:
-                            raise ValueError('incoming patients are not equal to scheduled + left')
+                            warnings.warn(f"incoming patients are not equal to scheduled + left, {iter}, {patgoing[0,:,tj,c,loc].sum()}, {left[c,loc]} , {pat[c,loc,i]}, {i}")
             elif c is 1:
                 if loc_dep is 1: # Location Dependent
                     patgoing[:,:,:,c,:], cancel[:,:,:,c,:],left[c,:]=allocateFlex_dep(pat[c,0,i], P[:,:,:,c,:], patgoing[:,:,:,c,:], cancel[:,:,:,c,:],left[c,:],data.r)
                     if patgoing[:, :,:, c, :].sum() + left[c, 0] != pat[c, 0, i]:
-                        raise ValueError('incoming patients are not equal to scheduled + left')
+                        warnings.warn(f"incoming patients are not equal to scheduled + left, {iter}, {patgoing[:, :, :, c, :].sum()}, {left[c, 0]}, {pat[c, 0, i]}, {i}")
                 else: # Location independent
                     for loc in range(data.dl):
                         patgoing[:,:,:,c,loc], cancel[:,:,:,c,loc],left[c,loc]=allocateFlex_ind(pat[c,loc,i], P[:,:,:,c,loc], patgoing[:,:,:,c,loc], cancel[:,:,:,c,loc],left[c,loc],data.r)
                         if patgoing[:, :, :,c, loc].sum() + left[c,loc].sum() != pat[c, loc, i].sum():
-                            raise ValueError('incoming patients are not equal to scheduled + left')
+                            warnings.warn(f"incoming patients are not equal to scheduled + left, {iter}, {patgoing[:, :, :, c, loc].sum()}, {left[c, loc]}, {pat[c, loc, i]}, {i}")
             else:
                 for loc in range(data.dl):
                     patgoing[:, c - 2,:, c, loc], cancel[:, c - 2,:, c, loc], left[c, loc] = allocateDedicated(
                         pat[c, loc, i], P[:, c - 2,:, c, loc], patgoing[:, c - 2,:, c, loc], cancel[:, c - 2,:, c, loc],
                         left[c, loc], data.r)
                     if patgoing[:,c-2,:,c,loc].sum() + left[c,loc] != pat[c,loc,i]:
-                        raise ValueError('incoming patients are not equal to scheduled + left')
+                        warnings.warn(f"incoming patients are not equal to scheduled + left, {iter}, {patgoing[:, c - 2, :, c, loc].sum()}, {left[c,loc]}, {pat[c,loc,i]}, {i}")
 
         patfinal = patgoing - cancel
         schedule[:, :,:,:, i, i:i + data.dd] = np.moveaxis(np.moveaxis(patfinal, 0, 4),1,3)
         patleft[i] = left.sum()
 
-    appSum = np.apply_over_axes(np.sum,schedule,axes=[4, 3,1]).squeeze()#schedule.sum(axis=2).sum(axis=1)
-    profit = np.apply_over_axes(np.sum, appSum, axes=[0,1]).squeeze() - data.theta*np.array([np.abs(appSum[:,:,kk] - data.C).sum() for kk in range(appSum.shape[2])])
+    appSum = schedule.sum(axis=(4, 3,1)).squeeze()#schedule.sum(axis=2).sum(axis=1)
+    if not data.joint:
+        profit = appSum.sum(axis=(0, 1)).squeeze() - data.theta*np.array(np.abs(appSum - data.C[:,:,None]).sum(axis=(0,1)))
+    else:
+        profit = appSum[:,:,:data.Tmax].sum(axis=(0, 1)).squeeze() - data.theta * np.abs(appSum[:,:,:data.Tmax] - C_temp).sum(axis=(0,1))
 
     return (profit[data.day0:data.Tmax].sum() / (data.Tmax - data.day0), patleft[data.day0:data.Tmax].sum()/(data.Tmax - data.day0), data.dat_C)
 
@@ -253,6 +259,8 @@ if __name__ == "__main__":
     parser.add_argument('-F', "--tempsuffix", help="Folder Suffix", default="")
     parser.add_argument('-I', "--ParImax", help="Should each iteration be in parallel", action='store_true')
     parser.add_argument('-z', "--folder", help="Folder to write the results")
+    parser.add_argument('-g', '--gamma', help="Gamma Scenario", type=float, nargs='+', default=[0.])
+    parser.add_argument('--beta', help="Beta Scenario for rebooking of canceled slots", type=float, nargs='+', default=[1.])
     args = parser.parse_args()
 
     dat = pd.read_csv(args.file)
@@ -262,7 +270,6 @@ if __name__ == "__main__":
     data.day0 = args.day0
     data.dk = dat.Provider.unique().shape[0]
     data.dt = args.nslots
-    data.CAP = 20
 
     data.dc = data.dk + 2
     data.dl = dat.Clinic.unique().shape[0]
@@ -278,13 +285,8 @@ if __name__ == "__main__":
     data.C = np.round(data.C,0)
     Cold = data.C.copy()
     data.csign = np.sign(data.C)
-    data.beta = np.zeros(data.dc)
-    data.beta[:2] = 0
-    data.gamma = np.ones(data.dk)
-
-
-    # step = 5
-
+    beta_def= np.sign(data.C)
+    gamma_def = np.sign(data.C)
     # horizon = range(start, end, step)
     demand = dat[["Provider", "Clinic", "Daily_Demand"]].pivot(index="Provider", columns="Clinic",
                                                                     values="Daily_Demand").fillna(0).as_matrix()
@@ -302,76 +304,78 @@ if __name__ == "__main__":
                                               prefix='_'.join(
                                                   args.policies),
                                               dir=data.tmpdir, delete=False)
+    for data.beta in [i*beta_def for i in args.beta]:
+        for data.gamma in [i*gamma_def for i in args.gamma]:
+            for dob in args.doubly:
+                l_original = calc_lambda(data,dob)
+                for data.joint in args.joint:
+                    for ch in args.choices:
+                        for data.dd in args.horizon:
+                            data.v = calculate_choice(data.dd, data.dk, data.dt,data.dc, data.dl,ch,[varray1, varray2])
+                            data.r = np.array([max(1 - 0.04 * j, 0.6) for j in range(data.dd)])
+                            for data.theta in args.theta:
+                                data.CAP = 12
+                                for data.dynamic in args.policies:
+                                    for loc_dep in args.locations:
+                                        data.Prob = np.zeros([data.dd, data.dk, data.dt, data.dc, data.dl])
+                                        results = None
+                                        data.l[1,:,:] = l_original.copy()
+                                        data.l[1, :, :] = data.l[1, :, :].sum(axis=0) if loc_dep is 1 else data.l[1, :, :]
+                                        # flex_data = float(data.l[1, 0, :]) if dob == 0 else list(data.l[1,0,:])
+                                        data.C = Cold.copy()
+                                        if data.dynamic == "DP":  # Dynamic
+                                            data.Prob = generateDailyProb(data, "SP",loc_dep=loc_dep)
+                                            if (data.Prob < 0).any() or (data.Prob > 1).any():
+                                                pd.DataFrame(np.array([data.theta, data.dd, ch, data.dynamic])).to_csv("error.csv")
+                                            if not args.ParImax:
+                                                pool = mp.Pool(processes=data.processes)
+                                                results = pool.map(functools.partial(run_main, data=data), range(args.Imax))
+                                            else:
+                                                results = run_main(args.Imax, data=data)
+                                        elif data.dynamic == "DCPP":
+                                            data.Prob = generateDailyProb(data, "CP", loc_dep=loc_dep)
+                                            if (data.Prob < 0).any() or (data.Prob > 1).any():
+                                                pd.DataFrame(np.array([data.theta, data.dd, ch, data.dynamic])).to_csv("error.csv")
+                                            if not args.ParImax:
+                                                pool = mp.Pool(processes=data.processes)
+                                                results = pool.map(functools.partial(run_main, data=data), range(args.Imax))
+                                            else:
+                                                results = run_main(args.Imax, data=data)
+                                        elif data.dynamic in ["SP","SPP","CP"]:  # Static
+                                            data.Prob = generateDailyProb(data, data.dynamic, 1,loc_dep=loc_dep, keepfiles=True)
+                                            # break
+                                            if (data.Prob < 0).any() or (data.Prob > 1).any():
+                                                pd.DataFrame(np.array([data.theta, data.dd, ch, data.dynamic])).to_csv("error.csv")
+                                            if not args.ParImax:
+                                                pool = mp.Pool(processes=data.processes)
+                                                results = pool.map(functools.partial(run_main, data=data), range(args.Imax))
+                                            else:
+                                                results = run_main(args.Imax, data=data)
 
-    for dob in args.doubly:
-        l_original = calc_lambda(data,dob)
-        for data.joint in args.joint:
-            for ch in args.choices:
-                for data.dd in args.horizon:
-                    data.v = calculate_choice(data.dd, data.dk, data.dt,data.dc, data.dl,ch,[varray1, varray2])
-                    data.r = np.array([max(1 - 0.04 * j, 0.6) for j in range(data.dd)])
-                    for data.theta in args.theta:
-                        for data.dynamic in args.policies:
-                            for loc_dep in args.locations:
-                                data.Prob = np.zeros([data.dd, data.dk, data.dt, data.dc, data.dl])
-                                results = None
-                                data.l[1,:,:] = l_original.copy()
-                                data.l[1, :, :] = data.l[1, :, :].sum(axis=0) if loc_dep is 1 else data.l[1, :, :]
-                                # flex_data = float(data.l[1, 0, :]) if dob == 0 else list(data.l[1,0,:])
-                                data.C = Cold.copy()
-                                if data.dynamic == "DP":  # Dynamic
-                                    data.Prob = generateDailyProb(data, "SP",loc_dep=loc_dep)
-                                    if (data.Prob < 0).any() or (data.Prob > 1).any():
-                                        pd.DataFrame(np.array([data.theta, data.dd, ch, data.dynamic])).to_csv("error.csv")
-                                    if not args.ParImax:
-                                        pool = mp.Pool(processes=data.processes)
-                                        results = pool.map(functools.partial(run_main, data=data), range(args.Imax))
-                                    else:
-                                        results = run_main(args.Imax, data=data)
-                                elif data.dynamic == "DCPP":
-                                    data.Prob = generateDailyProb(data, "CP", loc_dep=loc_dep)
-                                    if (data.Prob < 0).any() or (data.Prob > 1).any():
-                                        pd.DataFrame(np.array([data.theta, data.dd, ch, data.dynamic])).to_csv("error.csv")
-                                    if not args.ParImax:
-                                        pool = mp.Pool(processes=data.processes)
-                                        results = pool.map(functools.partial(run_main, data=data), range(args.Imax))
-                                    else:
-                                        results = run_main(args.Imax, data=data)
-                                elif data.dynamic in ["SP","SPP","CP"]:  # Static
-                                    data.Prob = generateDailyProb(data, data.dynamic, 1,loc_dep=loc_dep, keepfiles=True)
-                                    # break
-                                    if (data.Prob < 0).any() or (data.Prob > 1).any():
-                                        pd.DataFrame(np.array([data.theta, data.dd, ch, data.dynamic])).to_csv("error.csv")
-                                    if not args.ParImax:
-                                        pool = mp.Pool(processes=data.processes)
-                                        results = pool.map(functools.partial(run_main, data=data), range(args.Imax))
-                                    else:
-                                        results = run_main(args.Imax, data=data)
+                                        if results is not None:
+                                            if args.ParImax:
+                                                res = np.array(results[0])
+                                                rep_no = 1
+                                                run_no = [args.Imax]
+                                            else:
+                                                res = np.array([result[0] for result in results])
+                                                rep_no = args.Imax
+                                                run_no = np.arange(0,args.Imax)
 
-                                if results is not None:
-                                    if args.ParImax:
-                                        res = np.array(results[0])
-                                        rep_no = 1
-                                        run_no = [args.Imax]
-                                    else:
-                                        res = np.array([result[0] for result in results])
-                                        rep_no = args.Imax
-                                        run_no = np.arange(0,args.Imax)
-
-                                    # dat_C = pd.concat([result[2] for result in results])
-                                    # dat_C.to_csv(c_file, header=False
-                                    out = pd.DataFrame({'Run': run_no,'Horizon':np.repeat(data.dd, rep_no),
-                                                        'Theta': np.repeat(data.theta, rep_no),'Choice': np.repeat(ch, rep_no),
-                                                        'Policy': np.repeat(data.dynamic, rep_no),'Loc_Dep':np.repeat(loc_dep, rep_no),
-                                                        'Joint': np.repeat(data.joint, rep_no),'Doubly': np.repeat(dob, rep_no),
-                                                        'Profit': res})
-                                    if os.stat(result_file.name).st_size >0:
-                                        out.to_csv(result_file.name, header=False, mode='a', index=False)
-                                    else:
-                                        out.to_csv(result_file.name, header=True, mode='w', index=False)
-                                    data.l[1,:,:] = np.copy(l_original)
-                            # for ii in range(args.Imax):
-                            #    print(run_main(ii, data=data))
+                                            # dat_C = pd.concat([result[2] for result in results])
+                                            # dat_C.to_csv(c_file, header=False
+                                            out = pd.DataFrame({'Run': run_no,'Horizon':np.repeat(data.dd, rep_no),
+                                                                'Theta': np.repeat(data.theta, rep_no),'Choice': np.repeat(ch, rep_no),
+                                                                'Policy': np.repeat(data.dynamic, rep_no),'Loc_Dep':np.repeat(loc_dep, rep_no),
+                                                                'Joint': np.repeat(data.joint, rep_no),'Doubly': np.repeat(dob, rep_no),
+                                                                'Profit': res})
+                                            if os.stat(result_file.name).st_size >0:
+                                                out.to_csv(result_file.name, header=False, mode='a', index=False)
+                                            else:
+                                                out.to_csv(result_file.name, header=True, mode='w', index=False)
+                                            data.l[1,:,:] = np.copy(l_original)
+                                    # for ii in range(args.Imax):
+                                    #    print(run_main(ii, data=data))
     # outfile.close()
     # c_file.close()
     # pivot_tab = tempfile.NamedTemporaryFile(suffix='.csv',prefix='_'.join(policy) + 'pivot_table',dir=data.tmpdir, delete=False)

model_code1.py
@@ -104,13 +104,18 @@ P4D(dj,kj,lj,nj)
 
 Positive Variable x,u,C,alphaS,alphaD,P3S,P4S,P1D, P2D, P3D,P4D;
 Binary Variable cb;
-x.up(dj,kj,tj,cj,lj) = sign(v(dj,kj,tj,cj,lj));;
+x.up(dj,kj,tj,cj,lj) = sign(v(dj,kj,tj,cj,lj));
+u.up(cj,lj) = 1;
+*x.l(dj,kj,tj,cj,lj) = z(dj,kj,tj,cj,lj);
 *u.up(cj,lj) = 1;
 *u.lo(cj,lj) = 0;
 *u.up(cj,lj)$(ord(cj) eq 1) = 0;
 *u.lo(cj,lj)$(ord(cj) >= 3 and ord(cj) <=5 and ord(lj) eq 2) = 1;
 *u.lo(cj,lj)$(ord(cj) >=6 and ord(lj) eq 1) = 1;
 cb.up(kj,lj,i) = csign(kj,lj);
+cb.lo(kj,lj,i)$(ord(i) <= C_start(kj,lj)-8) = csign(kj,lj);
+*cb.lo(kj,lj,i)$(ord(i)<=7)  = csign(kj,lj);
+cb.up(kj,lj,i)$(ord(i)>= C_start(kj,lj) + 3) = 0;
 P1S.up(kj,lj,nj) = 1;
 P2S.up(kj,lj,nj) = 1;
 P3S.up(kj,lj,nj) = 1;
@@ -120,7 +125,7 @@ P2D.up(dj,kj,lj,nj) = 1;
 P3D.up(dj,kj,lj,nj) = 1;
 P4D.up(dj,kj,lj,nj) = 1;
 
-cb.l(kj,lj,i)$(ord(i)<= C_start(kj,lj)+1) = 1;
+*cb.l(kj,lj,i)$(ord(i)<= C_start(kj,lj)+1) = 1;
 
 *display u.lo, x.up;
 
@@ -172,7 +177,7 @@ P3eqS(kj,lj,nj)      .. P3S(kj,lj,nj) =e= exp(-alphaS(kj,lj,nj))*(sum(i$(ord(i)<
 P4eqS(kj,lj,nj)      .. P4S(kj,lj,nj) =e= exp(-alphaS(kj,lj,nj))*(sum(i$(ord(i)<=CAP-1), cb(kj,lj,i+2)*alphaS(kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
 
 *********** Dynamic
-alphaEqD(dj,kj,lj,nj)   .. alphaD(dj,kj,lj,nj) =e= sum((cj,di,dx)$((ord(dx) eq ord(di)+ord(dj)-1) and (ord(di) ge 1) and (ord(di) le dd +1 - ord(dj))),sum(tj,y(kj,cj,lj,tj,di,dx))*rt(dx,di)) + 
+alphaEqD(dj,kj,lj,nj)   .. alphaD(dj,kj,lj,nj) =e= 0.0001+sum((cj,di,dx)$((ord(dx) eq ord(di)+ord(dj)-1) and (ord(di) ge 1) and (ord(di) le dd +1 - ord(dj))),sum(tj,y(kj,cj,lj,tj,di,dx))*rt(dx,di)) + 
 sum(cj,L(cj,lj,nj)*r(dj)*sum(tj,x(dj,kj,tj,cj,lj))) + sum((cj,di)$((ord(di) ge 1) and (ord(di) le ord(dj)-1)),L(cj,lj,nj)*r(di)*sum(tj,z(di,kj,tj,cj,lj)));
 P1eqD(dj,kj,lj,nj)      .. P1D(dj,kj,lj,nj) =e= 1 - exp(-alphaD(dj,kj,lj,nj))*(sum(i$(ord(i)<=CAP), cb(kj,lj,i+1)*alphaD(dj,kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
 P2eqD(dj,kj,lj,nj)      .. P2D(dj,kj,lj,nj) =e= 1 - exp(-alphaD(dj,kj,lj,nj))*(sum(i$(ord(i)<=CAP+1), cb(kj,lj,i)*alphaD(dj,kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
@@ -184,19 +189,28 @@ MODEL SP_loc_dep /eq1,eq2_loc_dep_1,eq3,eq3_1,eq4,eq4_1,capCon1,capCon2,alphaEqS
 MODEL DP_loc_dep /eq1,eq2_loc_dep_1,eq3,eq3_1,eq4,eq4_1,capCon1,capCon2,alphaEqD,P1eqD,P2eqD,P3eqD,P4eqD, total_profit_DP/;
 
 options
-limrow = 0,limcol = 0,reslim = 3000,optcr = 0.0, sysout = off, solprint = off;
+limrow = 0,limcol = 0,reslim = 1000,optcr = 0.0, sysout = off, solprint = off;
 
 option 
-    minlp = sbb;
+    minlp = couenne, nlp=knitro;
 
-*DP_loc_dep.nodlim = 250000;
-DP_loc_dep.nodlim = 50000;
+DP_loc_dep.nodlim = 250000;
+*DP_loc_dep.nodlim = 20000;
 DP_loc_dep.optfile = 1;
 $onecho >sbb.opt
+subsolver knitro
 loginterval 15000
 loglevel 0
 memnodes 250000
-varsel 3
+nodesel 0
+intsollim 1
+$offecho
+
+$onecho>knitro.opt
+mip_heuristic 3
+mip_method 2
+mip_branchrule 2
+mip_selectrule 3
 $offecho
 
 solve DP_loc_dep using minlp maximizing obj;
@@ -319,6 +333,8 @@ Parameter
          rt(dj,dj)
          st(dj,dj) 
          C(kj,lj)
+         gm(kj,lj)
+         bt(kj,lj)
          csign(kj,lj)
          L(cj,lj,nj)
          prob(nj)
@@ -330,7 +346,7 @@ Parameter
 
 $if not set gdxincname $abort 'no include file name for data file provided'
 $gdxin %gdxincname%
-$load dj,kj,tj,cj,lj,nj,dd,theta,CAP,v,r,s,C,csign,L,prob, Policy,y,z, loc_dep
+$load dj,kj,tj,cj,lj,nj,dd,theta,CAP,v,r,s,C,gm,bt,csign,L,prob, Policy,y,z, loc_dep
 $gdxin
 
 rt(dj,di)$(ord(di) le ord(dj)) = max(0.6,1 - 0.04*(ord(dj) - ord(di)));
@@ -387,10 +403,10 @@ P3eqD(dj,kj,lj,nj)
 P4eqD(dj,kj,lj,nj)
 ;
 
-total_profit_SP         .. obj =e= sum((kj,lj,nj), prob(nj)*alphaS(kj,lj,nj)) - 
-                                   theta*sum((kj,lj,nj),prob(nj)*(alphaS(kj,lj,nj)*P1S(kj,lj,nj) - C(kj,lj)*P2S(kj,lj,nj) + C(kj,lj)*P3S(kj,lj,nj) - alphaS(kj,lj,nj)*P4S(kj,lj,nj)));
+total_profit_SP         .. obj =e= sum((kj,lj,nj), prob(nj)*alphaS(kj,lj,nj)*(csign(kj,lj)-bt(kj,lj)*(csign(kj,lj)-gm(kj,lj)))) - 
+                                   theta*sum((kj,lj,nj),prob(nj)*(alphaS(kj,lj,nj)*P1S(kj,lj,nj) - gm(kj,lj)*C(kj,lj)*P2S(kj,lj,nj) + gm(kj,lj)*C(kj,lj)*P3S(kj,lj,nj) - alphaS(kj,lj,nj)*P4S(kj,lj,nj)));
 total_profit_DP         .. obj =e= sum((dj,kj,lj,nj), alphaD(dj,kj,lj,nj)*prob(nj))-
-                                   theta*sum((dj,kj,lj,nj),prob(nj)*(alphaD(dj,kj,lj,nj))*P1D(dj,kj,lj,nj) - C(kj,lj)*P2D(dj,kj,lj,nj) + C(kj,lj)*P3D(dj,kj,lj,nj) - alphaD(dj,kj,lj,nj)*P4D(dj,kj,lj,nj));
+                                   theta*sum((dj,kj,lj,nj),prob(nj)*(alphaD(dj,kj,lj,nj))*P1D(dj,kj,lj,nj) - gm(kj,lj)*C(kj,lj)*P2D(dj,kj,lj,nj) + gm(kj,lj)*C(kj,lj)*P3D(dj,kj,lj,nj) - alphaD(dj,kj,lj,nj)*P4D(dj,kj,lj,nj));
 eq1(kj,cj,lj)$(ord(cj) >= 3 and ord(kj) eq ord(cj)-2)   .. u(cj,lj) + sum((dj,tj), x(dj,kj,tj,cj,lj)) =e= 1;
 eq2_loc_dep_1(cj)$(ord(cj) eq 2)      .. sum(lj, u(cj,lj) + sum((kj,dj,tj),x(dj,kj,tj,cj,lj))) =e= 1;
 eq3(dj,tj,cj,lj)$(ord(cj) eq 1 and ord(dj) eq 1)   .. sum(kj, x(dj,kj,tj,cj,lj)) =e= 1;
@@ -404,7 +420,7 @@ P2eqS(kj,lj,nj)      .. P2S(kj,lj,nj) =e= 1 - exp(-alphaS(kj,lj,nj))*(sum(i$(ord
 P3eqS(kj,lj,nj)      .. P3S(kj,lj,nj) =e= exp(-alphaS(kj,lj,nj))*(sum(i$(ord(i)<=ceil(C(kj,lj))), alphaS(kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
 P4eqS(kj,lj,nj)      .. P4S(kj,lj,nj) =e= exp(-alphaS(kj,lj,nj))*(sum(i$(ord(i)<=ceil(C(kj,lj))-sign(C(kj,lj))), alphaS(kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
 *********** Dynamic
-alphaEqD(dj,kj,lj,nj)   .. alphaD(dj,kj,lj,nj) =e= sum((cj,di,dx)$((ord(dx) eq ord(di)+ord(dj)-1) and (ord(di) ge 1) and (ord(di) le dd +1 - ord(dj))),sum(tj,y(kj,cj,lj,tj,di,dx))*rt(dx,di)) + 
+alphaEqD(dj,kj,lj,nj)   .. alphaD(dj,kj,lj,nj) =e= 0.000001+sum((cj,di,dx)$((ord(dx) eq ord(di)+ord(dj)-1) and (ord(di) ge 1) and (ord(di) le dd +1 - ord(dj))),sum(tj,y(kj,cj,lj,tj,di,dx))*rt(dx,di)) + 
 sum(cj,L(cj,lj,nj)*r(dj)*sum(tj,x(dj,kj,tj,cj,lj))) + sum((cj,di)$((ord(di) ge 1) and (ord(di) le ord(dj)-1)),L(cj,lj,nj)*r(di)*sum(tj,z(di,kj,tj,cj,lj)));
 P1eqD(dj,kj,lj,nj)      .. P1D(dj,kj,lj,nj) =e= 1 - exp(-alphaD(dj,kj,lj,nj))*(sum(i$(ord(i)<=floor(C(kj,lj))), alphaD(dj,kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
 P2eqD(dj,kj,lj,nj)      .. P2D(dj,kj,lj,nj) =e= 1 - exp(-alphaD(dj,kj,lj,nj))*(sum(i$(ord(i)<=floor(C(kj,lj))+1), alphaD(dj,kj,lj,nj)**(ord(i)-1)/gamma(ord(i)) ));
@@ -416,7 +432,7 @@ MODEL SP_loc_dep /eq1,eq2_loc_dep_1,eq3,eq3_1,eq4,eq4_1,alphaEqS,P1eqS,P2eqS,P3e
 MODEL DP_loc_dep /eq1,eq2_loc_dep_1,eq3,eq3_1,eq4,eq4_1,alphaEqD,P1eqD,P2eqD,P3eqD,P4eqD, total_profit_DP/;
 
 option 
-   NLP = conopt,
+   NLP = knitro,
     limcol = 0,
     solprint = off,
     sysout = off,
@@ -468,6 +484,8 @@ Elseif Policy eq 2,
         s = db.add_parameter_dc('s', [dj], "show up prob")
         L = db.add_parameter_dc('L', [cj, lj, nj], "arrival rate")
         C = db.add_parameter_dc('C', [kj, lj], "Capacity Constraint Parameter")
+        gm = db.add_parameter_dc('gm', [kj,lj], "1 - physician cancellation prob")
+        bt = db.add_parameter_dc('bt', [kj,lj], "1 - ")
         csign = db.add_parameter_dc('csign', [kj, lj], "Capacity Constraint Parameter")
         prob = db.add_parameter_dc('prob', [nj], "doubly stochastic prob")
         y = db.add_parameter_dc('y', [kj, cj, lj, tj, dj, dj], 'Current Schedule')
@@ -482,6 +500,10 @@ Elseif Policy eq 2,
             L.add_record([str(i + 1) for i in key]).value = val
         for key, val in np.ndenumerate(data.C):
             C.add_record([str(i + 1) for i in key]).value = val
+        for key, val in np.ndenumerate(data.gamma):
+            gm.add_record([str(i + 1) for i in key]).value = val
+        for key, val in np.ndenumerate(data.beta):
+            bt.add_record([str(i + 1) for i in key]).value = val
         for key, val in np.ndenumerate(data.csign):
             csign.add_record([str(i + 1) for i in key]).value = val
         for key, val in np.ndenumerate(data.probl):